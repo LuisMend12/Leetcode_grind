c145e22a6dd2b8457ea5d7b2617004a2603de4e0
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-02-16 12:38:44 -0500

    first commit

---
+++ 1718.c
@@ -0,0 +1,22 @@
+bool backtrack(int *arr ,int *visited, int size ,int  n, int index , int cnt){
+    if(cnt==n)return true;
+    for(int i =n;i>=1;i--){
+        int j = i==1?index:index+i;
+        if(visited[i] || j>=size || arr[index] || arr[j])continue;
+        arr[index] = arr[j] = i;
+        visited[i] = 1;
+        int next_index = index;
+        while(next_index<size && arr[next_index]) next_index++;
+        if(backtrack(arr , visited , size , n , next_index , cnt+1))return true;
+        arr[index] = arr[j] = visited[i] = 0;
+    }
+    return false;
+}
+int* constructDistancedSequence(int n, int* returnSize) {
+    int size = n*2-1;
+    int *arr = calloc(size , sizeof(int));
+    int *vis = calloc(n+1 , sizeof(int));
+    backtrack(arr , vis , size , n , 0 , 0);
+    *returnSize = size;
+    return arr;
+}
