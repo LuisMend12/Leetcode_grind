fbd1be550dcce6fd4a0943832bece54da4f42b62
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-03-03 09:58:27 -0500

    fix

---
+++ March 2025/2161.cpp
@@ -0,0 +1,19 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+ int* pivotArray(int* nums, int numsSize, int pivot, int* returnSize) {
+
+    result = [0] * len(nums);
+
+    left = 0;
+    
+    right  = len(nums) - 1;
+
+
+    for i, j in zip(range(len(nums), range(len(nums), -1, -1, -1)))
+
+
+    
+    
+    
+}
