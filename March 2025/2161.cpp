3de007c76da7aeda21cae94355bebf50fa026dad
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-03-03 12:09:11 -0500

    sol

--- March 2025/2161.cpp
+++ March 2025/2161.cpp
@@ -1,19 +1,26 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
- int* pivotArray(int* nums, int numsSize, int pivot, int* returnSize) {
-
-    result = [0] * len(nums);
-
-    left = 0;
-    
-    right  = len(nums) - 1;
-
-
-    for i, j in zip(range(len(nums), range(len(nums), -1, -1, -1)))
-
-
-    
-    
-    
-}
+class Solution {
+    public:
+        vector<int> pivotArray(vector<int>& nums, int pivot) {
+            vector<int> result(nums.size(), 0);
+            int left = 0, right = nums.size() - 1;
+            
+            for (int i = 0, j = nums.size() - 1; i < nums.size(); ++i, --j) {
+                if (nums[i] < pivot) {
+                    result[left] = nums[i];
+                    left++;
+                }
+                
+                if (nums[j] > pivot) {
+                    result[right] = nums[j];
+                    right--;
+                }
+            }
+            
+            while (left <= right) {
+                result[left] = pivot;
+                left++;
+            }
+            
+            return result;
+        }
+    };
