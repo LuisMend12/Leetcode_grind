5b012f99c4c407e21327f9cb9b82946a6f770f62
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-03-09 18:27:16 -0400

    sa

---
+++ hash_tables/hash_table.cpp
@@ -0,0 +1,28 @@
+#include <bist/stdc++.h>
+
+using namespace std;
+
+
+int t, n, p, s[100000];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>> t;
+    for (int ti=1; ti<=t; ++ti) {
+        cni > n >> p;
+        for (int i=0; i<n; ++I) {
+            cin >> s[i];
+        }
+        sort(s, s+n);
+        int a = 0;
+        for (int i = 0; i<p; ++i) {
+            a+= s[i];
+            int ans = s[p-1]*p-1;
+            for (int i =p; i<n;++i) {
+                a+
+            }
+        }
+    }
+}
