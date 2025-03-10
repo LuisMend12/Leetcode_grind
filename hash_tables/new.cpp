a90461b072aeb64386414250382082416b0b45b9
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-03-10 11:49:35 -0400

    new

---
+++ hash_tables/new.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+
+struct Hash {
+    int BUCKET;
+
+    vector<vector<int>> table;
+
+    void insertItem(int key) {
+        int index = hashFunction(key);
+        table[index].push_back(key);
+    }
+
+    void deleteItem(int key);
+
+    int hashFunction(int x) return (x % BUCKET);
+
+    void displayHash();
+
+    Hash(int b) {
+        this->BUCKET = b;
+        table.resize(BUCKET);
+    }
+};
+
+void Hash::deleteItem(int key) {
+    int index = hashFunction(key);
+
+    auto it = find(table[index].begin(), table[index].end(), key);
+    if (it != table[index].end()) table[index].erase(it);
+}
+
+void Hash::displayHash() {
+    for (int i = 0; i < BUCKET; i++) {
+        cout << i;
+        for (int x : table[i]) cout << " --> " << x;
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<int> a = {15, 11, 27, 8, 12};
+
+    Hash h(7);
+    for (int key : a) h.insertItem(key);
+
+
+    h.deleteItem(12);
+
+    h.displayHash();
+
+    return 0;
+}
