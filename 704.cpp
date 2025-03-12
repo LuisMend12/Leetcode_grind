b299f020bed69e56fbd66a1c98d8eaab7aa50990
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-03-12 17:49:36 -0400

    new

---
+++ 704.cpp
@@ -0,0 +1,8 @@
+class Solution {
+    public:
+        int search(vector<int>& nums, int target) {
+            int idx = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+            return (idx >= nums.size() || nums[idx] != target) ? -1 : idx;
+    
+        }
+    };
