a704ae2f1a64422a0f0cf0f38259b283699ec57f
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-03-14 17:54:13 -0400

    fix

--- March 2025/2226.cpp
+++ March 2025/2226.cpp
@@ -1,21 +1,22 @@
 class Solution {
     public:
         int maximumCandies(vector<int>& candies, long long k) {
-            long long left = 0, right = *max(candies.begin(), candies.end());
+            long long left = 1, right = *max_element(candies.begin(), candies.end());
+    
             int result = 0;
-
-
+    
             while (left <= right) {
-                long long mid  = left + (right - left) / 2;
+                long long mid = left + (right - left) / 2;
                 long long children_count = 0;
-
-                for (int pile : candies) children_count += pile/mid;
-
+    
+                for (int pile : candies) children_count += pile / mid;
+    
                 if (children_count >= k) {
-                    resutl = mid;
+                    result = mid;
                     left = mid + 1;
                 } else right = mid - 1;
             }
             return result;
+    
         }
     };
