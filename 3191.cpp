6e4b0c9c0737663c47b833daa90ba470dcf57e4f
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-03-19 11:47:34 -0400

    new

---
+++ 3191.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int minOperations(vector<int>& nums) {
+            int n = nums.size();
+            int count = 0;
+            for (int i = 0; i<n-2; i++) {
+                if (nums[i] == 0) {
+                    count++;
+                    nums[i] = 1;
+                    nums[i+1] = 1 - nums[i+1];
+                    nums[i+2] = 1 - nums[i+2];
+                }
+            }
+            return nums[n-1]==1 && nums[n-2]==1 ? count : -1;
+        }
+    };
