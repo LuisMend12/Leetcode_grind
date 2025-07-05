8499e33603b328e482e77a9d2f940a3332a07c91
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-07-05 14:55:57 -0400

    optimal solution for 1431.py found

--- July_2025/1431.py
+++ July_2025/1431.py
@@ -2,12 +2,14 @@
 Not optimal solution
 '''
 
-
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candies = max(candies)
+
+        # added max_candies to reduce space complexity
         result = []
         for c in candies:
-            if c + extraCandies >= max(candies):
+            if c + extraCandies >= max_candies:
                 result.append(True)
             else:
                 result.append(False)
