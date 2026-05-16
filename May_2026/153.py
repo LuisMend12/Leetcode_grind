e5f85eadddae58210c6c8e9f1597c79b92b32e98
LuisMend12 <128239393+LuisMend12@users.noreply.github.com>
2026-05-15 21:27:42 -0400

    another solution

--- May_2026/153.py
+++ May_2026/153.py
@@ -21,9 +21,23 @@ class Solution:
         while left < right:
             mid = left + (right - left) // 2
 
-            if nums[mid] > nums[right]:
+            # if nums[mid] > nums[right]:
+            #     left = mid + 1
+            # else:
+            #     right = mid
+
+            # ----------------------
+
+
+            # ANOTHER SOLUTION 
+
+
+            if nums[0] <= nums[mid]:
                 left = mid + 1
+
             else:
                 right = mid
+
+
 
         return nums[left]
