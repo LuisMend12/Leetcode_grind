5c63c84c340902c2a5a1350a0ae97720f562ef33
LuisMend12 <128239393+LuisMend12@users.noreply.github.com>
2026-06-06 21:55:12 -0400

    new

--- June_2026/2196.py
+++ June_2026/2196.py
@@ -7,14 +7,6 @@
 # The test cases will be generated such that the binary tree is valid.
 
 
-from typing import List, Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
         nodes = {}
